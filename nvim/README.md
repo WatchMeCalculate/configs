90703dc2a6323b614cfb183e8e375355551b666a
Jonah Jolley <jonah@devetry.com>
2023-04-27 00:12:21 -0600

    remove background settings

--- nvim/README.md
+++ nvim/README.md
@@ -4,8 +4,10 @@
 
 1. Packer:
 
-```git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-~/.config/nvim/site/pack/packer/start/packer.nvim
+```
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 ```
 
 2. tree-sitter
--- nvim/lua/watchme/background.lua
+++ nvim/lua/watchme/background.lua
@@ -1,8 +1,9 @@
-vim.opt.background = 'dark'
-vim.opt.termguicolors = true
+-- vim.opt.background = 'dark'
+-- vim.opt.termguicolors = true
+-- vim.cmd('colorscheme solarized')
 vim.opt.cursorline = true
 vim.cmd([[
-  highlight CursorLine guibg=Black
+  highlight CursorLine guibg=Grey30
   highlight clear SignColumn
   highlight clear LineNr
 ]])
